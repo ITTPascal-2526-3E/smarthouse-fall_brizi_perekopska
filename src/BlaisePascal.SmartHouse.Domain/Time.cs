7d7ee93f9cbffc0d2be3f016d22f55e985fc1e46
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2025-10-31 09:25:21 +0100

    Added class Time

---
+++ src/BlaisePascal.SmartHouse.Domain/Time.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    internal class Time
+    {
+        private int Hours{ get; set;}
+        private int Minutes { get; set;}
+        private int Seconds { get; set;}
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            if(hours<=23 && hours>=0)
+            {
+                Hours = hours;
+            }
+
+            if (minutes <= 59 && minutes >= 0)
+            {
+                Minutes = minutes;
+            }
+
+            if (seconds <= 59 && seconds >= 0)
+            {
+                Seconds = seconds;
+            }
+        }
+    }
+}
