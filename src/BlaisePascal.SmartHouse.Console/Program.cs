3d8e2c264b62a7543481cac46874073b798edd61
Oleewna <lenusickot@gmail.com>
2025-11-03 13:01:55 +0100

    Update Program.cs
    Created Lamp1, EcoLamp1 and TwoLampDevice1 in program.cs, which names and brightness will be displayed in the console(not completed yet)

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 namespace BlaisePascal.SmartHouse.Domain
 {
 
@@ -7,9 +8,15 @@ namespace BlaisePascal.SmartHouse.Domain
     {
         static void Main(string[] args)
         {
+            EcoLamp EcoLamp1 = new EcoLamp(true, 50, "EcoLED", 10,77, new Time(10, 00, 00), new Time(12, 00, 00);
             Lamp Lamp1 = new Lamp(true, 100, [100, 10, 50], "LED", 20.0999, new Time(23, 23, 23), new Time(10, 10, 12));
-            Lamp1.Brightness = 70;
-            Console.WriteLine(Lamp1.Brightness);
+            TwoLampDevice TwoLampDevice1 = new TwoLampDevice();
+            do {
+                Console.Clear();
+                Console.WriteLine("Lamp1: "+ Lamp1.IsOn + ", brightness = " + Lamp1.Brightness);
+                Console.WriteLine("EcoLamp1: "+ EcoLamp1.IsOn + ", brightness = " + EcoLamp1.Brightness);
+                Console.WriteLine("TwoLampDevice1: "+ TwoLampDevice1.IsOn + ", brightness = " + TwoLampDevice1.Brightness);
+            } while(true);
         }
     }
 }
