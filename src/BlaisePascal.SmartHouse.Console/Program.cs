21e5affd7be616d4dfae4e16cb6e14beeb68ceaf
Oleewna <lenusickot@gmail.com>
2025-11-03 10:59:44 +0100

    Made a test in program.cs, changing brightness of new Lamp1

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -7,8 +7,9 @@ namespace BlaisePascal.SmartHouse.Domain
     {
         static void Main(string[] args)
         {
-            
-           
+            Lamp Lamp1 = new Lamp(true, 100, [100, 10, 50], "LED", 20.0999, new Time(23, 23, 23), new Time(10, 10, 12));
+            Lamp1.Brightness = 70;
+            Console.WriteLine(Lamp1.Brightness);
         }
     }
 }
