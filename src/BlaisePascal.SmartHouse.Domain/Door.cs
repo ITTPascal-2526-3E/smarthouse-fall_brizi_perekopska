d40ec7d6a5b07ebe82e7100296d63d87a5898f68
sibillaFall <sibillafall08@gmail.com>
2025-11-11 13:54:42 +0100

    Implemented class Door

---
+++ src/BlaisePascal.SmartHouse.Domain/Door.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    internal class Door
+    {
+        private bool IsLocked;
+
+        public Door(bool isClosed)
+        {
+            IsLocked = isClosed;
+        }
+
+
+        public bool LockUnlockTheDoor()
+        {
+            if(IsLocked == true)
+                IsLocked = false;
+            else
+                IsLocked = true;
+
+            return IsLocked;
+
+        }
+    }
+}
