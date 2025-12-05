9148086cb196e366b211347649911be42cd89e9e
BriziGiacomo <giacomaglio2009@gmail.com>
2025-12-05 09:10:53 +0100

    Program debugged

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -15,8 +15,8 @@ namespace BlaisePascal.SmartHouse.Domain
         static void Main(string[] args)
         {
             //Illumination Devices
-            Lamp Lamp1 = new Lamp(false, 100, [100, 10, 50], "LED", new Time(23, 23, 23), new Time(10, 10, 12));
-            EcoLamp EcoLamp1 = new EcoLamp(false, 50, "EcoLED", new Time(10, 00, 00), new Time(12, 00, 00), new Time(00, 00, 016));
+            Lamp Lamp1 = new Lamp("porta",false, 100, [100, 10, 50], "LED", new Time(23, 23, 23), new Time(10, 10, 12));
+            EcoLamp EcoLamp1 = new EcoLamp("portaEco",false, 50, "EcoLED", new Time(10, 00, 00), new Time(12, 00, 00), new Time(00, 00, 016));
             TwoLampDevice TwoLampDevice1 = new TwoLampDevice(Lamp1, EcoLamp1);
             //Temperature Devices
             Thermostat Thermostat1 = new Thermostat("Thermostat1", true, 19.5f, 23.5f);
