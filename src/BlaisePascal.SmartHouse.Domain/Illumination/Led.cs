3498e239e10bac9ef6f74d3ff2796409e0f18329
BriziGiacomo <giacomaglio2009@gmail.com>
2025-12-09 13:53:45 +0100

    Implemented class Led and MatrixLed, draf of the test

---
+++ src/BlaisePascal.SmartHouse.Domain/Illumination/Led.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.UsefulClasses;
+
+namespace BlaisePascal.SmartHouse.Domain.Illumination
+{
+    public class Led: Device
+    {
+        // Attributes
+        public byte Brightness { get; private set; }
+        public byte[] Color=new byte[3] { 0,0,0}; //RGB
+        const byte MaxBrightness = 100;
+        const byte MinBrightness = 1;
+        private byte BrightnessBeforeTurnOff;
+
+        //Constructor
+        public Led(string name, bool isOn,byte brightness, byte[] color): base(name,isOn) 
+        {
+            try
+            {
+                if (brightness >= MinBrightness && brightness <= MaxBrightness)
+                {
+                    Brightness = brightness;
+                    BrightnessBeforeTurnOff = Brightness;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        //Turn on the led.
+        public bool TurnOn()
+        {            
+            IsOn = true;
+            Brightness = BrightnessBeforeTurnOff;
+            return IsOn;
+        }
+        //Turn offthe led.
+        public bool TurnOff() 
+        {
+            IsOn = false;
+            Brightness = 0;
+            return IsOn;
+        }
+
+        //Change the brightness of the led.
+        public void ChangeBrightness(byte newBrightness)
+        {
+            try
+            {
+                if (newBrightness >= MinBrightness && newBrightness <= MaxBrightness)
+                {
+                    Brightness = newBrightness;
+                    BrightnessBeforeTurnOff = Brightness;                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        // Changes the color of the  lamp
+        public void ChangeLampColor(byte[] colors)
+        {
+            Color = colors;
+            Console.WriteLine($"The lamp color is changed to RGB({Color[0]}, {Color[1]}, {Color[2]})");
+        }
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.Domain/Illumination/MatrixLed.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.UsefulClasses;
+
+namespace BlaisePascal.SmartHouse.Domain.Illumination
+{
+    public class MatrixLed
+    {
+        //Attributes:
+        public Led[,] _MatrixLed { get; private set; }
+        private int Columns;
+        private int Rows;
+
+        //Constructor:
+        public MatrixLed(int columns, int rows)
+        {
+            try
+            {
+                if (Columns > 0)
+                {
+                    Columns = columns;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+                if (Rows > 0)
+                {
+                    Rows = rows;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("There is an error:");
+                Console.WriteLine(ex.Message);
+            }
+            for(int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < Rows; c++)
+                {
+                    _MatrixLed[c, r] = CreateMatrix();
+                }
+            }
+        }
+        //Prende i valori delle lampade in input per metterli sccessivamente nella matrice.
+        private Led CreateMatrix()
+        {
+            string name = Console.ReadLine() ?? "aaa";
+            bool isOn = Convert.ToBoolean(Console.ReadLine());
+            byte brightness = Convert.ToByte(Console.ReadLine());
+            byte[] colors = new byte[3];
+            for (int col = 0; col < 3; col++)
+            {
+                byte color = Convert.ToByte(Console.ReadLine());
+                colors[col] = color;
+            }
+            Led _Led = new Led(name, isOn, brightness, colors);
+
+            return _Led;
+        }
+
+        //Turn on al the led.
+        public void SwitchOnAll()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < Rows; c++)
+                {
+                    _MatrixLed[r, c].TurnOn();
+                }
+            }
+        }
+        //Turn off all the led.
+        public void SwitchOffAll()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < Rows; c++)
+                {
+                    _MatrixLed[r, c].TurnOff();
+                }
+            }
+        }
+
+        //Change the intensity of oll the led in a one specified.
+        public void SetIntensityAll(byte intensity)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < Rows; c++)
+                {
+                    _MatrixLed[r, c].ChangeBrightness(intensity);
+                }
+            }
+        }
+
+        //Return the led.
+        public Led GetLed(int row, int column)
+        {
+            return _MatrixLed[row, column];
+        }
+        //Return the row of led.
+        public Led[] GetLedInRow(int row)
+        {
+            Led[] leds= new Led[Rows];
+            for (int c = 0; c < Columns; c++)
+            {
+                leds[c]=_MatrixLed[row, c];
+            }
+            return leds;
+        }
+        //Return the columnsof the led.
+        public Led[] GetLedInColumn(int column)
+        {
+            Led[] leds = new Led[Columns];
+            for (int r = 0; r < Rows; r++)
+            {
+                leds[r] = _MatrixLed[r, column];
+            }
+            return leds;
+        }
+    }
+}
