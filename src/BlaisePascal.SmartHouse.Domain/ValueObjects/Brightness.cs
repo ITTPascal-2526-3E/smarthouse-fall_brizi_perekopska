20ff0d526490f062550ddb5507717ee361a1cc12
Oleewna <lenusickot@gmail.com>
2026-02-03 13:31:13 +0100

    Added value objects folder + brightness class

---
+++ src/BlaisePascal.SmartHouse.Domain/ValueObjects/Brightness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.ValueObjects
+{
+    public sealed class Brightness
+    {
+        public int Value { get; private set; }
+
+        public const int Min = 0;
+        public const int Max = 100;
+
+        private Brightness(int value)
+        {
+            Value = value;
+        }
+    }
+}
