9eeea5bd156004b9da7e708bb3cbe0c2dadc5751
Oleewna <lenusickot@gmail.com>
2026-03-03 13:15:05 +0100

    Added Infrastructure

---
+++ src/BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Illumination/Lamps/CsvLampRepository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Illumination.Lamps
+{
+    public class CsvLampRepository : ILampRepository
+    {
+        private readonly string _filePath = "lamps.csv";
+        public CsvLampRepository()
+        {
+            var solutionRoot = LocalPathHelper.GetSolutionRoot();
+
+            var dataFolder = Path.Combine(solutionRoot, "data");
+            Directory.CreateDirectory(dataFolder);
+
+            _filePath = Path.Combine(dataFolder, "lamps.csv");
+
+            if (!File.Exists(_filePath))
+            {
+                Save(new List<Lamp>());
+            }
+        }
+
+        public List<Lamp> GetAll()
+        {
+            return Load();
+        }
+
+        public Lamp GetById(Guid id)
+        {
+            return Load().FirstOrDefault(lamp => lamp.Id == id);
+        }
+
+        public void Add(Lamp lamp)
+        {
+            var lamps = Load();
+            lamps.Add(lamp);
+            Save(lamps);
+        }
+
+        public void Remove(Guid id)
+        {
+            var lamps = Load();
+            var lamp = lamps.First(l => l.Id == id);
+            lamps.Remove(lampToRemove);
+            Save(lamps);
+        }
+
+        public void Update(Lamp lamp)
+        {
+            var lamps = Load();
+            var index = lamps.FindIndex(l => l.Id == lamp.Id);
+            if (index == -1)
+                throw new Exception("Lamp not found");
+        }
+
+        private void Save(List<Lamp> lamps)
+        {
+            var dtos = lamps;
+            var lines = new List<string>
+            {
+                "Id,Name,IsOn,Brightness,ColorR,ColorG,ColorB,Type,OnTime,OffTime"
+            };
+
+            foreach (var dto in dtos)
+            {
+                lines.Add(string.Join(",",
+                    dto.Id,
+                    dto.Name,
+                    dto.IsOn,
+                    dto.Brightness,
+                    dto.Color.R,
+                    dto.Color.G,
+                    dto.Color.B,
+                    dto.Type,
+                    $"{dto.OnTime.Hours.Value}:{dto.OnTime.Minutes.Value}:{dto.OnTime.Seconds.Value}",
+                    $"{dto.OffTime.Hours.Value}:{dto.OffTime.Minutes.Value}:{dto.OffTime.Seconds.Value}"
+                ));
+            }
+
+            File.WriteAllLines(lines);
+        }
+
+        private List<Lamps> Load()
+        {
+            var lines = File.ReadAllLines(_filePath);
+
+            if (lines.Length <= 1)
+                return new List<Lamp>();
+
+            var lamps = new List<Lamp>();
+
+            foreach (var line in lines.Skip(1)) //skips header
+            {
+                var values = line.Split(',');
+                var dto = new Lamp
+                {
+                    Id = Guid.Parse(values[0]),
+                    Name = new Name(values[1]),
+                    IsOn = bool.Parse(values[2]),
+                    Brightness = byte.Parse(values[3]),
+                    Color = Color.From(byte.Parse(values[4]), byte.Parse(values[5]), byte.Parse(values[6])),
+                    Type = values[7],
+                    OnTime = Time.From(TimeSpan.Parse(values[8])),
+                    OffTime = Time.From(TimeSpan.Parse(values[9]))
+                };
+
+                lamps.Add();
+            }
+            return lamps;
+        }
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Illumination/Lamps/InMemoryLampRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using BlaisePascal.SmartHouse.Domain.Illumination;
+using BlaisePascal.SmartHouse.Domain.ValueObjects;
+
+namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Illumination.Lamps
+{
+    public class InMemoryLampRepository : ILampRepository
+    {
+        private readonly List<Lamp> _lamps;
+
+        public InMemoryLampRepository()
+        {
+            _lamps = new List<Lamp>
+            {
+                new Lamp("Name"),
+                new Lamp("Name2")
+            };
+        }
+
+        public List<Lamp> GetAll()
+        {
+            return _lamps;
+        }
+
+        public Lamp GetById(Guid id)
+        {
+            return _lamps.FirstOrDefault(lamp => lamp.Id == id);
+        }
+
+        public void Add(Lamp lamp)
+        {
+            if (lamp == null)
+                throw new ArgumentNullException(nameof(lamp));
+            _lamps.Add(lamp);
+        }
+
+        public void Remove(Guid id)
+        {
+            var lamp = GetById(id);
+            if (lamp != null)
+                _lamps.Remove(lamp);
+        }
+
+        public void Update(Lamp lamp)
+        {
+            // Not to do
+        }
+    }
+}
